97260541bb0f553af31c90476b2d4b2c8f36f694
alejandro <alejandro.barraza@alumnos.ucn.cl>
2020-11-17 08:43:55 -0300

    feat(Class): add the new class

---
+++ app/src/main/java/cl/ucn/disc/dsm/abarraza/news/model/News.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2020 alejandro barraza alejandro.barraza@alumnos.ucn.cl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package cl.ucn.disc.dsm.abarraza.news.model;
+
+import org.threeten.bp.ZonedDateTime;
+
+/**
+ * the domain model news
+ * @author alejandro barraza
+ */
+public final class News {
+    /**
+     * unique id
+     */
+    private long id;
+    /**
+     * the title
+     */
+    private String title;
+    /**
+     * the source
+     */
+    private String source;
+    /**
+     * the author
+     */
+    private String author;
+    /**
+     * the url
+     */
+    private String url;
+    /**
+     * the url of image
+     */
+    private String urlImage;
+    /**
+     * the description
+     */
+    private String description;
+    /**
+     * the content
+     */
+    private String content;
+    /**
+     * the date of publish
+     */
+    private ZonedDateTime publishedAt;
+
+    /**
+     * constructor
+     * @param id
+     * @param title
+     * @param source
+     * @param author
+     * @param url
+     * @param urlImage
+     * @param description
+     * @param content
+     * @param publishedAt
+     */
+
+    public News(long id, String title, String source, String author, String url, String urlImage, String description, String content, ZonedDateTime publishedAt) {
+        this.id = id;
+        this.title = title;
+        this.source = source;
+        this.author = author;
+        this.url = url;
+        this.urlImage = urlImage;
+        this.description = description;
+        this.content = content;
+        this.publishedAt = publishedAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public ZonedDateTime getPublishedAt() {
+        return publishedAt;
+    }
+}
