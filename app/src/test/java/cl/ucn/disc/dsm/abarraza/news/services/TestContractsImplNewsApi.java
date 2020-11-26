ff92f9ae124309492065996064b9055ee2ae8171
alejandro <alejandro.barraza@alumnos.ucn.cl>
2020-11-26 10:10:12 -0300

    feat(Clases): news Test class for fetching Api

---
+++ app/src/test/java/cl/ucn/disc/dsm/abarraza/news/services/TestContractsImplNewsApi.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2020 alejandro barraza alejandro.barraza@alumnos.ucn.cl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package cl.ucn.disc.dsm.abarraza.news.services;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import cl.ucn.disc.dsm.abarraza.news.model.News;
+
+/**
+ * testing with api service
+ */
+public class TestContractsImplNewsApi {
+    /**
+     * the logger
+     */
+    private static final Logger log = LoggerFactory.getLogger(TestContractsImplNewsApi.class);
+
+    /**
+     * the test
+     */
+    @Test
+    public void testRetrieveNews(){
+        System.out.println("testing");
+        //the api
+        String apiKey = "9d57a60918974e93bd6ffe30710629a5";
+
+        Contracts contracts = new ContractcImplNewsApi(apiKey);
+
+        List<News> news = contracts.retrieveNews(10);
+        Assertions.assertNotNull(news,"list null");
+        Assertions.assertEquals(10,news.size(),"wrong size");
+        for(News n : news){
+            System.out.println("news" + ToStringBuilder.reflectionToString(
+                    n , ToStringStyle.MULTI_LINE_STYLE
+            ));
+        }
+        System.out.println("done");
+    }
+}
