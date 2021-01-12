9311899907fbb1b6a53a6a963a46419cd5d19d23
Diego Calderon <diego.claderon@alumnos.ucn.cl>
2021-01-12 01:08:44 -0300

    feat(AppDatabase): add AppDatabase
    Se creo la clase AppDatabase para trabajar con room

---
+++ app/src/main/java/cl/ucn/disc/dsm/abarraza/news/database/AppDatabase.java
@@ -0,0 +1,29 @@
+package cl.ucn.disc.dsm.abarraza.news.database;
+
+import android.content.Context;
+
+import androidx.room.Room;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import android.content.Context;
+
+@Database(entities = {}, version = 1)
+
+public abstract class AppDatabase extends RoomDatabase{
+
+    public static AppDatabase INSTANCE;
+
+    public static AppDatabase getInstance(Context context) {
+        if(INSTANCE == null){
+            INSTANCE = Room.databaseBuilder(context, AppDatabase.class, "checklist.db")
+                    .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+
+        return INSTANCE;
+    }
+}
