3184aee455dd5bd770d62b74cf84c23d66ea9c9d
Diego Calderon <diego.claderon@alumnos.ucn.cl>
2021-01-17 19:14:18 -0300

    feat(ItemDAO)
    Se adapto ItemDAO a Item

--- app/src/main/java/cl/ucn/disc/dsm/abarraza/news/database/dao/ItemDAO.java
+++ app/src/main/java/cl/ucn/disc/dsm/abarraza/news/database/dao/ItemDAO.java
@@ -1,7 +1,10 @@
 package cl.ucn.disc.dsm.abarraza.news.database.dao;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -13,6 +16,15 @@ public interface ItemDAO {
     @Query("select * from Item")
     List<Item> getAll();
 
-    @Query("select * from Item where itemId = :itemId")
-    Item findById(int itemId);
+    @Query("select * from Item where id = :id")
+    Item findById(int id);
+
+    @Insert
+    void insert(Item item);
+
+    @Update
+    void update (Item item);
+
+    @Delete
+    void delete (Item item);
 }
