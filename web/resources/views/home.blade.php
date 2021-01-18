2da2d127122660fa17d12631a52fa4e932f88a4f
Nicolae <nsm006@alumnos.ucn.cl>
2021-01-18 17:33:50 -0300

    feat(buttons)
    improvements on the home.blade.php

--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -1,10 +1,4 @@
-@extends('layouts.app')
-
-@section('content')
-    <title>Home</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-    <!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -16,11 +10,11 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;1,300&display=swap" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
-    <title>Add News</title>
+    <title>Bienvenido a "The News"</title>
 </head>
 
 
-<body>
+<!--<body>
 <nav class="navbar navbar-dark bg-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
@@ -28,14 +22,78 @@
             News
         </a>
     </div>
+
 </nav>
+
+<body>-->
+
+<!-- NavBar -->
+<div class="navBar2" id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #000000">
+        <!-- Logo News -->
+        <a class="navbar-brand" href="{{ url('/')}}">
+            <img src="/img/icon.png" alt="" width="30" height="30" class="d-inline-block align-top icon">
+            News
+        </a>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Lado derecho de la barra de navegación -->
+            <ul class="nav navbar-nav ml-auto">
+                <!-- Links de autenticación -->
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">Register <span class="sr-only"></span></a>
+                </li>
+
+                @guest
+
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('home') }}">Home <span class="sr-only"></span></a>
+                    </li>
+
+                    <!-- Si el usuario es un administrador... -->
+                    @if(Auth::user()->rol === 'Administrador' )
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('user.index') }}">Administrar usuarios<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('user.index') }}">Administrar usuarios<span class="sr-only">(current)</span></a>
+                        </li>
+                @endif
+
+                <!-- CERRAR SESIÓN -->
+                    <div class="dropdown">
+                        <button style="color: rgba(255,255,255,.75)" class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </button>
+
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <!-- El botón activa el formulario que permite el logout -->
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                Salir
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </div>
+                    </div>
+            @endguest
+
+        </div>
+    </nav>
+</div>
+@auth
+
+@endauth
 
 <!-- home -->
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Menu') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -46,6 +104,21 @@
 
                     {{ __('You are logged in!') }}
                 </div>
+
+                <div class="form-group">
+                    <div class="col-sm-10">
+                        <button type="submit" class="btn btn-dark">Ir a "Register News"</button>
+                    </div>
+                </div>
+
+                <button href="{{ route('home') }}" type="button" class="btn btn-dark">Home</button>
+
+                <div class="form-group">
+                    <div class="col-sm-10">
+                        <button type="submit" class="btn btn-dark">Listado de Noticias</button>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
@@ -65,5 +138,3 @@
 
 </body>
 </html>
-
-@endsection
