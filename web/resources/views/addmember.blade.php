ce06fc1787cfa82fba5258dee25ce2bd40140921
Nicolae <nsm006@alumnos.ucn.cl>
2021-01-19 17:07:18 -0300

    refactor(addMember) addMember.blade.php has been erased

--- web/resources/views/addmember.blade.php
+++
@@ -1,8 +0,0 @@
-<h1>Add Members</h1>
-<form action="" method="POST" >
-    @csrf
-    <input type="text" name="name" placeholder="Enter Name"> <br> <br>
-    <input type="text" name="email" placeholder="Enter Email"> <br> <br>
-    <input type="text" name="address" placeholder="Enter Address"> <br> <br>
-    <button type="submit" >Add Member</button>
-</form>
