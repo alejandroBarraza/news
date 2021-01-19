310dfa9c0f7b7a02ff7fba8a416612ad903fef8c
Nicolae <nsm006@alumnos.ucn.cl>
2021-01-18 22:49:33 -0300

    feat(listNews) added a new version of the list of news

---
+++ web/resources/views/listnews.blade.php
@@ -0,0 +1,89 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;1,300&display=swap" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://markcell.github.io/jquery-tabledit/assets/js/tabledit.min.js"></script>
+    <title>Tabla News</title>
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="/img/icon.png" alt="" width="30" height="30" class="d-inline-block align-top icon">
+            News
+        </a>
+    </div>
+</nav>
+
+<div class="container">
+    <br />
+    <h3 align="center">Lista de todas las Noticias</h3>
+    <br />
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Registro de Noticias</h3>
+        </div>
+        <div class="panel-body">
+            <div class="table-responsive">
+                @csrf
+                <table id="editable" class="table table-bordered table-striped">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Titulo</th>
+                        <th>Autor</th>
+                        <th>Fuente</th>
+                        <th>Url</th>
+                        <th>Url de la Imagen</th>
+                        <th>Descripcion</th>
+                        <th>Contenido</th>
+                        <th>Fecha</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($news as $news)
+                        <tr>
+                            <td>{{ $news['id']}}</td>
+                            <td>{{ $news['title']}}</td>
+                            <td>{{ $news['author']}}</td>
+                            <td>{{ $news['source']}}</td>
+                            <td>{{ $news['url']}}</td>
+                            <td>{{ $news['urlImage']}}</td>
+                            <td>{{ $news['description']}}</td>
+                            <td>{{ $news['content']}}</td>
+                            <td>{{ $news['date']}}</td>
+                            <td>
+                                <a href="{{ "edit/".$news['id']}}" class="btn btn-dark">Editar</a>
+                                <a href="{{ "delete/".$news['id']}}" class="btn btn-danger">Eliminar</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
+
+<footer>
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3 footer-text">Desarrollo de Soluciones Moviles II-2020:
+        <a style="color: #000000" href="https://www.ucn.cl/"> Universidad Católica del Norte</a>
+    </div>
+    <!-- Copyright -->
+</footer>
