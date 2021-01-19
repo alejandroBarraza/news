40b91672bb4465b6a9963c63dccf5348d43b6ba6
Nicolae <nsm006@alumnos.ucn.cl>
2021-01-18 22:46:02 -0300

    fix(addNews) fixed the content form

--- web/resources/views/addnews.blade.php
+++ web/resources/views/addnews.blade.php
@@ -88,8 +88,8 @@
         <div class="form-group ">
             <label for="content" class="col-sm-2 col-form-label">Content</label>
             <div>
-                <input type="text" class="form-control" name="content" placeholder="content" >
-            <!--<textarea type="text" class="form-control" name="content" rows="4" cols="40" value="{{old('content')}}"></textarea> -->
+                <!--<input type="text" class="form-control" name="content" placeholder="content" -->
+            <textarea type="text" class="form-control" name="content" rows="4" cols="40" value="{{old('content')}}"></textarea>
             </div>
 
         </div>
