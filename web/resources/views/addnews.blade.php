28f9f0202d729896bd939f1078e90f2b42d4cc32
Nicolae <nsm006@alumnos.ucn.cl>
2021-01-19 21:20:24 -0300

    feat(addNews) implementation of a new navbar, new message validator and the hour of publication of the news

--- web/resources/views/addnews.blade.php
+++ web/resources/views/addnews.blade.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -10,51 +11,85 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;1,300&display=swap" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
-    <title>Add News</title>
+    <title>Registrar Noticias</title>
 </head>
 
 
 <body>
-<nav class="navbar navbar-dark bg-dark">
+<!--<nav class="navbar navbar-dark bg-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
             <img src="/img/icon.png" alt="" width="30" height="30" class="d-inline-block align-top icon">
             News
         </a>
     </div>
-</nav>
+</nav>-->
+
+<!-- NavBar -->
+<div class="navBar2" id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #000000">
+        <!-- Logo News -->
+        <a class="navbar-brand" href="{{ url('/')}}">
+            <img src="/img/icon.png" alt="" width="30" height="30" class="d-inline-block align-top icon">
+            News
+        </a>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Lado derecho de la barra de navegación -->
+            <ul class="nav navbar-nav ml-auto">
+                <!-- Links de autenticación -->
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/listnews">Lista de Noticias</a>
+
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
 
 
 <div class="container container-form">
-    <h1>Register News</h1>
+    <h1>Registrar Noticias</h1>
+
+    @if($errors -> any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors-> all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form action="#" method="POST">
 
         @csrf
 
         <div class="form-group">
-            <label for="title" class="col-sm-2 ">Title</label>
+            <label for="title" class="col-sm-2 ">Titulo</label>
             <div>
 
-                <input type="text" class="form-control" name="title" placeholder="Title"
+                <input type="text" class="form-control" name="title" placeholder="Inserte un titulo"
                 >
             </div>
 
         </div>
 
         <div class="form-group">
-            <label for="source" class="col-sm-2 col-form-label">Source</label>
+            <label for="author" class="col-form-label">Autor</label>
             <div>
 
-                <input type="text" class="form-control" name="source" placeholder="Source"
+                <input type="text" class="form-control" name="author" placeholder="Inserte un autor"
                 >
             </div>
         </div>
 
         <div class="form-group">
-            <label for="author" class="col-form-label">Author</label>
+            <label for="source" class="col-sm-2 col-form-label">Fuente</label>
             <div>
 
-                <input type="text" class="form-control" name="author" placeholder="Author"
+                <input type="text" class="form-control" name="source" placeholder="Inserte la fuente de la noticia"
                 >
             </div>
         </div>
@@ -63,43 +98,43 @@
             <label for="url" class="col-sm-2 col-form-label">URL</label>
             <div>
 
-                <input type="text" class="form-control" name="url" placeholder="url of the news">
+                <input type="text" class="form-control" name="url" placeholder="Inserte la url de la noticia ">
             </div>
         </div>
 
         <div class="form-group">
-            <label for="urlImage" class="col-sm-2 col-form-label">URL Image</label>
+            <label for="urlImage" class="col-sm-2 col-form-label">URL de la Imagen</label>
 
             <div>
 
-                <input type="text" class="form-control" name="urlImage" placeholder="url of the image" >
+                <input type="text" class="form-control" name="urlImage" placeholder="Inserte la url de la imagen de la noticia" >
             </div>
         </div>
 
         <div class="form-group">
-            <label for="description" class="col-sm-2 col-form-label">Description</label>
+            <label for="description" class="col-sm-2 col-form-label">Descripcion</label>
             <div>
 
-                <input type="text" class="form-control" name="description" placeholder="description of the news" >
+                <input type="text" class="form-control" name="description" placeholder="Inserte una descripcion de la noticia" >
 
             </div>
         </div>
 
         <div class="form-group ">
-            <label for="content" class="col-sm-2 col-form-label">Content</label>
+            <label for="content" class="col-sm-2 col-form-label">Contenido</label>
             <div>
                 <!--<input type="text" class="form-control" name="content" placeholder="content" -->
-            <textarea type="text" class="form-control" name="content" rows="4" cols="40" value="{{old('content')}}"></textarea>
+            <textarea type="text" class="form-control" name="content" rows="4" cols="40" value="{{old('content')}}" placeholder="Inserte el contenido de la noticia"></textarea>
             </div>
 
         </div>
 
         <?php
         date_default_timezone_set('America/Santiago');
-        $date_actual = date("d-m-Y");
+        $date_actual = date("d-m-Y H:i:s");
         ?>
         <div class="form-group">
-            <label for="date" class="col-sm-2 col-form-label">Published</label>
+            <label for="date" class="col-sm-2 col-form-label">Fecha de Publicacion</label>
             <div>
                 <input type="text" class="form-control" name="date" type="datetime" value="<?= $date_actual?>">
             </div>
