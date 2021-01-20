1380bcdcdd0a24f825e2fed7385a7b8688d26939
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-01-19 23:47:02 -0300

    refecator(LaravelNews):refactor class for laravelApi

--- app/src/main/java/cl/ucn/disc/dsm/abarraza/news/retrofit/LaravelNews.java
+++ app/src/main/java/cl/ucn/disc/dsm/abarraza/news/retrofit/LaravelNews.java
@@ -3,9 +3,49 @@ package cl.ucn.disc.dsm.abarraza.news.retrofit;
 import org.threeten.bp.ZonedDateTime;
 //testing with JsonPlaceholder until waiting api rest from laravel.
 public class LaravelNews {
-    private int userId;
     private int id;
-    private String title;
-    private String body;
+    private int title;
+    private String author;
+    private String source;
+    private String url;
+    private String urlImage;
+    private String description;
+    private String content;
+    private String date;
 
+    public int getId() {
+        return id;
+    }
+
+    public int getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getDate() {
+        return date;
+    }
 }
