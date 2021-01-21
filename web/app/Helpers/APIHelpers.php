898be4144a242218a164b2e5ba8f846b84bc88d5
Nicolae <nsm006@alumnos.ucn.cl>
2021-01-20 22:11:08 -0300

    perf(apiHelper): returns the json content in a simple way

--- web/app/Helpers/APIHelpers.php
+++ web/app/Helpers/APIHelpers.php
@@ -7,30 +7,11 @@ class APIHelpers {
     /**
      * Creates an API response from the data.
      *
-     * @param $is_error
-     * @param $code
-     * @param $message
      * @param $content
      * @return array
      */
-    public static function createAPIResponse($is_error, $code, $message, $content){
+    public static function createAPIResponse($content){
 
-        $result = [];
-
-        if($is_error){
-            $result['success'] = false;
-            $result['success'] = $code;
-            $result['success'] = $message;
-        }else{
-            $result['success'] = true;
-            $result['success'] = $code;
-            if($content == null){
-                $result['message'] = $message;
-            }else{
-                $result['data'] = $content;
-            }
-        }
-
-        return $result;
+        return $content;
     }
 }
